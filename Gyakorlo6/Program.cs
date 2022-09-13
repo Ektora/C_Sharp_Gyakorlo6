87bdb465feb0e1deb124e09e8df000fe1534d2ca
Ektora <zsoltlucski@gmail.com>
2022-09-13 21:22:10 +0200

    Prefix feladat kész

--- Gyakorlo6/Program.cs
+++ Gyakorlo6/Program.cs
@@ -11,6 +11,11 @@
 
             Console.WriteLine("Nagybetűk száma: " + UpperCaseLetterInString("AbcDEFef1g"));
             Console.WriteLine("Számjegyek száma: " + DigitInString("Abcdefgh123"));
+
+            string szoveg1, szoveg2;
+            szoveg1 = Console.ReadLine();
+            szoveg2 = Console.ReadLine();
+            Console.WriteLine($"{szoveg1} és {szoveg2} közös metszete: {prefix(szoveg1,szoveg2)}");
         }
 
         public static Boolean PasswordCheck(string password)
@@ -50,6 +55,22 @@
                     szam++;
             }
             return szam;
+        }
+
+        public static String prefix(String s1, String s2)
+        {
+            int rovidebbSzovegHossza = s1.Length < s2.Length ? s1.Length : s2.Length;
+            int egyezik = 0;
+            for(int i = 0; i < rovidebbSzovegHossza; i++)
+            {
+                if (s1[i].Equals(s2[i]))
+                    egyezik++;
+            }
+            if(egyezik > 0)
+            {
+                return s1.Substring(0, egyezik);
+            }
+            return String.Empty;
         }
     }
 }
